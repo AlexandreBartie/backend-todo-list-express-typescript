8baa30d2173ff566850a01d2b1bde14a8f35e001
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-03-02 03:08:33 -0300

    feat: settings eslint and prettier

--- server.ts
+++ server.ts
@@ -1,22 +1,22 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from 'express'
 
 import dotenv from 'dotenv'
 
 //
 // Instantiate exppress app
 const app = express()
-dotenv.config();
+dotenv.config()
 
 //
 // Define server port
-const port = process.env.PORT;
+const port = process.env.PORT
 
 //
 // Create a default route
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server v#0.0.2');
+  res.send('Express + TypeScript Server v#0.0.2')
 })
 
 //
 // Listen the request on the defined port
-app.listen(port);
+app.listen(port)
