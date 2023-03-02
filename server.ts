c03f6df8aa5c54bb5357f76404e2ea919e416664
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-03-02 02:17:53 -0300

    feat: add env file to setting api-server

--- server.ts
+++ server.ts
@@ -1,17 +1,20 @@
 import express, { Request, Response } from 'express';
 
+import dotenv from 'dotenv'
+
 //
 // Instantiate exppress app
 const app = express()
+dotenv.config();
 
 //
 // Define server port
-const port = 3200
+const port = process.env.PORT;
 
 //
 // Create a default route
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server v#0.0.1');
 })
 
 //
