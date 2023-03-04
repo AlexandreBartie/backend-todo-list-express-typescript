d8c6f5395d314f413085636580a6dae10c683240
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-03-03 22:12:13 -0300

    feat: install typeORM

--- server.ts
+++ server.ts
@@ -1,11 +1,24 @@
 import express, { Request, Response } from 'express'
 
 import dotenv from 'dotenv'
+import { DataSource } from 'typeorm'
 
 //
 // Instantiate exppress app
 const app = express()
 dotenv.config()
+
+//
+// Create DatabaseConnection
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: false,
+})
 
 //
 // Define server port
@@ -19,4 +32,12 @@ app.get('/', (req: Request, res: Response) => {
 
 //
 // Listen the request on the defined port
-app.listen(port)
+AppDataSource.initialize().then(() => {
+  // Listen the request on the defined port
+  app.listen(port)
+  console.log('Data Source has been initialized!')
+}).catch((err) => {
+    console.error('Error during Data Source initialization', err)
+})
+
+
