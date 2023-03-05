d0a1ed9814fa7153a7e2027d2dc73fd2426d35bc
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-03-05 01:07:31 -0300

    feat: add task entity

---
+++ src/task/task.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Priority } from '../enums/Priority'
+import { Status } from '../enums/Status'
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({
+    type: 'varchar',
+    length: 255
+  })
+  title: string
+
+  @Column({
+    type: 'longtext'
+  })
+  description: string
+
+  @Column({
+    type: 'enum',
+    enum: Priority,
+    default: Priority.normal
+  })
+  priority: Priority
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.todo
+  })
+  status: Status
+
+}
