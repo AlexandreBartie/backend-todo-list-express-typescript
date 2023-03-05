62fcb1a625c4fbe728829091db5eddf18f1f2618
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-03-05 01:08:04 -0300

    fix: data-source settings

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,19 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Task } from "./tasks/tasks.entity"
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { Task } from './task/task.entity'
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "user",
-    password: "password",
-    database: "Todo",
-    synchronize: true,
-    logging: false,
-    entities: [Task],
-    migrations: [],
-    subscribers: [],
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'user',
+  password: 'password',
+  database: 'Todo',
+  synchronize: true,
+  logging: false,
+  entities: [Task],
+  migrations: [],
+  subscribers: [],
 })
 
 console.log(process.env.MYSQL_USER)
